c2cb2e457e58aa886ab56290089ebc9021eff1ee
Lex <whityga@yandex.ru>
2024-09-18 14:57:01 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int mile = 20; // сколько стоит 1 миля
+
+        int bilet = 3000; // стоимость билета
+
+
+        System.out.println("Рассчитали количество бонусных миль. На один билет вышло:");
+        System.out.println((bilet/mile) + " бонусных миль");
+    }
+}
